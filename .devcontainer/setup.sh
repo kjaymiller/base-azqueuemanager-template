a7831fe5f980c215e1b712dbd33f67cbc0a364df
Jay Miller <kjaymiller@gmail.com>
2022-12-14 18:38:06 -0800

    update setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 python -m pip install --upgrade pip
-python -m pip install pip-tools
-python -m piptools compile -o requirementst.txt pyproject.toml
-python -m pip install -r requirements.txt
+python -m pip install --user pip-tools
+python -m piptools compile -o requirements.txt pyproject.toml
+python -m pip install --user -r requirements.txt
